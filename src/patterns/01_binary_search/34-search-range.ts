d8ef267edbcea1d2e6fc27994818bedc70ccbc83
y0M0ST <phucnguyen2812cuwin@gmail.com>
2026-08-17 23:28:22 +0700

    refactor: optimize algorithm for search range and its work good

--- src/patterns/01_binary_search/34-search-range.ts
+++ src/patterns/01_binary_search/34-search-range.ts
@@ -6,19 +6,20 @@
  */
 
 export function searchRange(nums: number[], target: number): number[] {
-  const findBound = (isFirst: boolean): number => {
+  const findBound = (isFirst: boolean): number=> {
     let left: number = 0
-    let right: number = nums.length - 1
-    let result: number = -1
+    let right: number = nums.length - 1 //Because in array the first element has index 0
+    let result = -1 //Default we can't not find anything
     while (left <= right) {
-      let mid = Math.floor((right + left) / 2)
+      let mid: number = Math.floor((right + left) / 2)
+      //(7+0) / 2 = 3,5 => So i use method floor of Math to set 3.5 to 3
       if (nums[mid] < target) {
-        left = mid + 1
+        left = mid + 1 //move to right
       } else if (nums[mid] > target) {
-        right = mid - 1
+        right = mid - 1 //move to left
       } else {
-        result = mid
-        if (isFirst) {
+        result = mid //We don't return mid here like like traditional Binary Search algorithm
+        if (isFirst) { //And at here we countinue find the target number because we not sure that the target number is only one in array
           right = mid - 1
         } else {
           left = mid + 1
@@ -29,11 +30,7 @@ export function searchRange(nums: number[], target: number): number[] {
   }
 
   const firstPos = findBound(true)
-  if (firstPos === -1) {
-    return [-1, -1]
-  }
-
+  if (firstPos === -1) return [-1, -1]
   const lastPos = findBound(false)
-
   return [firstPos, lastPos]
 }
