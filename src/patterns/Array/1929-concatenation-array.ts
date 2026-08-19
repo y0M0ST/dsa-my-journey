50f50c5feaed60370877f783f9aff0b01dda62ce
y0M0ST <phucnguyen2812cuwin@gmail.com>
2026-08-19 22:25:27 +0700

    feat(Array)(1929): add another way to solved it

--- src/patterns/Array/1929-concatenation-array.ts
+++ src/patterns/Array/1929-concatenation-array.ts
@@ -6,6 +6,15 @@
  */
 export function concatenationArray(nums: number[]): number[] {
   //Method 1. Oh yeahhh, with JS/TS I use spread oprator, I solve this problem in a single line of code
-  return [...nums, ...nums]
+  // return [...nums, ...nums]
+  // :)))) uh-huhhh so very very fastttt, but coding interview, i dont use this way :v
+  // Method 2. I use classic way
+  let newArr: number[] = []
+  for (let i = 0; i < nums.length; i++) {
+    newArr[i] = nums[i]!
+    newArr[i + nums.length]! = nums[i]!
+  }
+  return newArr
+  // alright, that so basic !!!
 
 }
