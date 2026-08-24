fd841f7501c2151971389e8cb2031e2b52f4f17e
y0M0ST <phucnguyen2812cuwin@gmail.com>
2026-08-24 14:58:02 +0700

    refactor(testCase): fix testcase for LC153

--- src/patterns/Binary_Search/153-find-min.test.ts
+++ src/patterns/Binary_Search/153-find-min.test.ts
@@ -3,12 +3,12 @@ import { findMin } from './153-find-min.js';
 
 describe('Pattern: Binary_Search -> 153-find-min', () => {
   const testCases = [
-    { input: [5,6,7,0,1,2,3,4], expected: 0 },
-    { input: [1,3,4,5], expected: 1 },
+    { input: [5, 6, 7, 0, 1, 2, 3, 4], expected: 0 },
+    { input: [1, 3, 4, 5], expected: 1 },
     { input: [], expected: 0 },
     { input: [1], expected: 1 },
-    { input: [3,1], expected: 1 },
-    { input: [4,5,6,1,2,3,7,8,9], expected: 1 },
+    { input: [3, 1], expected: 1 },
+    { input: [4, 5, 6, 7, 8, 9, 1, 2, 3,], expected: 1 },
   ];
 
   it.each(testCases)(
